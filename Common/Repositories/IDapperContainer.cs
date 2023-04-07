d97a0d4c077ad3bc7e4a12a2b5abd064215dce27
phuocleoceo <ht10082001@gmail.com>
2023-04-07 23:44:19 +0700

    Init

---
+++ Common/Repositories/IDapperContainer.cs
@@ -0,0 +1,26 @@
+using System.Data;
+using Dapper;
+
+namespace PlcBase.Common.Repositories;
+
+public interface IDapperContainer : IDisposable
+{
+    Task<T> Single<T>(DapperQuery dapperQuery);
+
+    Task Execute(DapperQuery dapperQuery);
+
+    Task<T> OneRecord<T>(DapperQuery dapperQuery);
+
+    Task<IEnumerable<T>> List<T>(DapperQuery dapperQuery);
+
+    Task<Tuple<IEnumerable<T1>, IEnumerable<T2>>> List<T1, T2>(DapperQuery dapperQuery);
+}
+
+public class DapperQuery
+{
+    public string Query { get; set; } = null;
+
+    public DynamicParameters Params { get; set; } = null;
+
+    public CommandType CommandType { get; set; } = CommandType.Text;
+}
