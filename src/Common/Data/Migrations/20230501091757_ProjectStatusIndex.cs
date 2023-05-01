4d2419a2e906445b0ee970a0878d20db3c95ebc7
phuocleoceo <ht10082001@gmail.com>
2023-05-01 16:18:57 +0700

    Projetc Status Index

---
+++ src/Common/Data/Migrations/20230501091757_ProjectStatusIndex.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace plcbase.Common.Data.Migrations
+{
+    public partial class ProjectStatusIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "index",
+                table: "project_status",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "index",
+                table: "project_status");
+        }
+    }
+}
