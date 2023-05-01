5150688a608eeb455f22b7d6cc7a9f5562491036
phuocleoceo <ht10082001@gmail.com>
2023-05-01 12:09:01 +0700

    Improve mapper

--- src/Common/Data/Mapper/MapperExtension.cs
+++ src/Common/Data/Mapper/MapperExtension.cs
@@ -17,17 +17,19 @@ public static class MapperExtension
 {
     public static void ConfigureAutoMapper(this IServiceCollection services)
     {
-        services.AddAutoMapper(typeof(AuthMapping));
-        services.AddAutoMapper(typeof(UserMapping));
-        services.AddAutoMapper(typeof(AddressMapping));
-        services.AddAutoMapper(typeof(RolePermissionMapping));
-        services.AddAutoMapper(typeof(ConfigSettingMapping));
-        services.AddAutoMapper(typeof(MediaMapping));
-        services.AddAutoMapper(typeof(ProjectMapping));
-        services.AddAutoMapper(typeof(ProjectMemberMapping));
-        services.AddAutoMapper(typeof(ProjectStatusMapping));
-        services.AddAutoMapper(typeof(InvitationMapping));
-        services.AddAutoMapper(typeof(SprintMapping));
-        services.AddAutoMapper(typeof(IssueMapping));
+        services.AddAutoMapper(
+            typeof(AuthMapping),
+            typeof(UserMapping),
+            typeof(AddressMapping),
+            typeof(RolePermissionMapping),
+            typeof(ConfigSettingMapping),
+            typeof(MediaMapping),
+            typeof(ProjectMapping),
+            typeof(ProjectMemberMapping),
+            typeof(ProjectStatusMapping),
+            typeof(InvitationMapping),
+            typeof(SprintMapping),
+            typeof(IssueMapping)
+        );
     }
 }
