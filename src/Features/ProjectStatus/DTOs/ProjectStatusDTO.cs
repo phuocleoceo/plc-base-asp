b56d6e04c9b65b8a2f5b1c027ef06c137b82114e
phuocleoceo <ht10082001@gmail.com>
2023-05-30 21:19:22 +0700

    Project Status Id for DTO

--- src/Features/ProjectStatus/DTOs/ProjectStatusDTO.cs
+++ src/Features/ProjectStatus/DTOs/ProjectStatusDTO.cs
@@ -2,7 +2,7 @@ namespace PlcBase.Features.ProjectStatus.DTOs;
 
 public class ProjectStatusDTO
 {
+    public int Id { get; set; }
     public string Name { get; set; }
-
     public double Index { get; set; }
 }
